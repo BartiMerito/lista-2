cb45abf3f60aab82a98c67d9d2c335d1a3d8574e
BartiMerito <152792538+BartiMerito@users.noreply.github.com>
2024-05-26 13:26:54 +0200

    Update providers.tf

---
+++ terraform-s3/terraform/providers.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region                      = "us-east-1"
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  s3_force_path_style         = true
+  endpoints {
+    s3 = "http://localhost:4566"
+  }
+}
